05e954a26bb0bace049f3e1e3830ba3054981f3e
yjx20123 <1835271170@qq.com>
2021-12-22 17:02:46 +0800

    12.22

--- Blog_demo/src/main/java/com/yang/blog/controller/admin/ImageApi.java
+++ Blog_demo/src/main/java/com/yang/blog/controller/admin/ImageApi.java
@@ -1,9 +1,26 @@
 package com.yang.blog.controller.admin;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yang.blog.response.ResponseResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+
+@RequestMapping("/admin/image")
 public class ImageApi {
+    @PostMapping
+    public ResponseResult uploadImage(){
+        return ResponseResult.SUCCESS();
+    }@DeleteMapping("{imageId}")
+    public ResponseResult deleteImage(@PathVariable("imageId") String iamgeId){
+        return ResponseResult.SUCCESS();
+    }@PutMapping("{imageId}")
+    public ResponseResult updateImage(@PathVariable("imageId") String iamgeId){
+        return ResponseResult.SUCCESS();
+    }@GetMapping("imageId")
+    public ResponseResult getImage(@PathVariable("imageId") String iamgeId){
+        return ResponseResult.SUCCESS();
+    }@PostMapping
+    public ResponseResult Image(){
+        return ResponseResult.SUCCESS();
+    }
 }
